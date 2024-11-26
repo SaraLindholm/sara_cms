09b1604f309ab74a6fbfa125a47e2bfed66a13ad
SaraLindholm <sara.lindholm88@gmail.com>
2024-11-26 15:44:13 +0100

    gjorde om bilderna till thumbnails

--- task7/nyheter.php
+++ task7/nyheter.php
@@ -7,46 +7,85 @@ sl_get_header();
 <div class="container d-flex flex-wrap">
   <div class="left d-flex flex-wrap">
     <div class="card">
-      <img src="https://cdn.pixabay.com/photo/2019/06/06/13/52/six-4256065_640.jpg" class="card-img-top" alt="car interiour">
+      <a target="_blank" href="/src/car.png">
+      <img src="/src/car.png" class="card-img-top" alt="car interiour"></a>
       <div class="card-body">
-        <h5 class="card-title"><a href="#">Man voltade med bil- 2 skadade till sjukhus.</a></h5>
+        <h3 class="card-title"><a href="#">Man voltade med bil- 2 skadade till sjukhus.</a></h3>
           <h6 class="card-subtitle mb-2 text-body-secondary">date</h6>
         <p class="card-text">Det var under gårdagen som en man i hög hastighet voltade i viadukten på avfart 12 på E6an i riktning mot Malmö. Inga andra bilar var inblandade i ...</p>
       </div>
     </div>
     <div class="card">
-      <img src="https://cdn.pixabay.com/photo/2016/11/03/13/42/meditation-1794292_640.jpg" class="card-img-top" alt="...">
+    <a target="_blank" href="/src/monk.png">
+      <img src="/src/monk.png" class="card-img-top" alt="..."></a>
       <div class="card-body">
-        <h5 class="card-title"><a href="#">Kloster i Thailand granskas för oortodoxa metoder</a></h5>
+        <h3 class="card-title"><a href="#">Kloster i Thailand granskas för oortodoxa metoder</a></h3>
           <h6 class="card-subtitle mb-2 text-body-secondary">date</h6>
         <p class="card-text">Ett kloster i Thailand har hamnat i blåsväder efter anklagelser om att de använt döda kroppar i meditationsövningar. Myndigheterna har inlett en utredning</p>
       </div>
     </div>
     <div class="card">
-      <img src="https://cdn.pixabay.com/photo/2017/11/29/11/03/kids-2985782_640.jpg" class="card-img-top" alt="...">
+      <a target="_blank" href="/src/aiLearing.png">
+      <img src="/src/aiLearing.png" class="card-img-top" alt="..."></a>
       <div class="card-body">
-        <h5 class="card-title"><a href="#">AI i Utbildning:</a></h5>
+        <h3 class="card-title"><a href="#">AI i Utbildning:</a></h3>
           <h6 class="card-subtitle mb-2 text-body-secondary">date</h6>
         <p class="card-text">En ny AI-plattform anpassar lektioner till individuella behov och revolutionerar klassrum världen över. Lärare får nu detaljerad uppföljning av varje elevs framsteg.</p>
       </div>
     </div>
     <div class="card">
-      <img src="https://cdn.pixabay.com/photo/2017/08/01/00/24/panel-2562240_640.jpg" class="card-img-top" alt="...">
+    <a target="_blank" href="/src/greenEnergy.png">
+      <img src="/src/greenEnergy.png" class="card-img-top" alt="..."></a>
       <div class="card-body">
-        <h5 class="card-title"><a href="#">Grön Energi-Breakthrough:</a></h5>
+        <h3 class="card-title"><a href="#">Grön Energi-Breakthrough:</a></h3>
           <h6 class="card-subtitle mb-2 text-body-secondary">date</h6>
         <p class="card-text">En ny solpanelsteknik har ökat effektiviteten och gjort förnybar energi mer tillgänglig för utvecklingsländer..</p>
       </div>
     </div>
     <div class="card">
-      <img src="https://cdn.pixabay.com/photo/2022/09/16/23/06/mars-7459788_640.jpg" class="card-img-top" alt="...">
+    <a target="_blank" href="/src/space.png">
+      <img src="/src/space.png" class="card-img-top" alt="space"></a>
       <div class="card-body">
-        <h5 class="card-title"><a href="#">Mars Koloni Färdig inom 10 år:</a></h5>
+        <h3 class="card-title"><a href="#">Mars Koloni Färdig inom 10 år:</a></h3>
           <h6 class="card-subtitle mb-2 text-body-secondary">date</h6>
         <p class="card-text">Forskare tror att människor kan bosätta sig på Mars inom ett decennium. Forskningen om hållbara bostäder och resurshantering går snabbt framåt.</p>
       </div>
     </div>
   </div>
-  <div class="right">test</div>
+  <div class="right">
+  <div class="cardRight">
+      <div class="card-body">
+        <h3 class="card-title">Senaste Uppdateringar</h3>
+          <h6 class="card-subtitle mb-2 text-body-secondary">date</h6>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+      </div>
+  </div>
+  <div class="cardRight">
+      <div class="card-body">
+        <h3 class="card-title">Lokala Nyheter</h3>
+          <h6 class="card-subtitle mb-2 text-body-secondary">hook filter $stad</h6>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+      </div>
+  </div>
+  <div class="cardRight">
+      <div class="card-body">
+        <h3 class="card-title">Mest Läst Väst</h3>
+          <h6 class="card-subtitle mb-2 text-body-secondary">date</h6>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+        <p class="card-text">...</p>
+      </div>
+  </div>
+</div>
 </div>
 <?php sl_get_footer(); ?>
