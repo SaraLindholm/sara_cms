b90bffa61438b9029336b4e606922ee103f9de49
SaraLindholm <sara.lindholm88@gmail.com>
2024-11-26 16:47:47 +0100

    byggt en framsida

--- task7/404.php
+++ task7/404.php
@@ -1,4 +1,5 @@
 <?php include 'helper.php';
 sl_get_header();
+echo '<h4>404: Sidan kunde inte hittas! Kanske har den gömts under en osynlighetsmantel, eller så försvann den genom en portnyckel. Leta vidare, trollstav redo!</h4>';
 sl_get_footer();
 ?>
--- task7/parts/footer.php
+++ task7/parts/footer.php
@@ -1,15 +1,13 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-
 </body>
-<footer class="bg-secondary ps-3 p-3 bg-opacity-50 border-bottom">
+<footer class="bg-dark text-white py-3">
+  <div class="container text-center">
+    <p>&copy; 2024 Nyheter i Väst. Alla rättigheter reserverade.</p>
+    <nav>
+      <a href="/kontakt" class="text-white me-3">Kontakt</a>
+      <a href="/om-oss" class="text-white">Om oss</a>
+    </nav>
+  </div>
+<!-- <footer class="bg-secondary ps-3 p-3 bg-opacity-50 border-bottom">
 
   <div class="hstack gap-3">
     <div class="p-2">First item
@@ -29,35 +27,7 @@
       <p>1</p>
       </div>
     </div>
-  </div>
+  </div> -->
 
-    <!-- <div class="container text-center">
-      <div class="row align-items-end">
-        <div class="col">
-          One of three columns
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-          </ul>
-        </div>
-        <div class="col">
-          One of three columns
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-          </ul>
-        </div>
-        <div class="col">
-          One of three columns
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-          </ul>
-        </div>
-      </div>
-    </div> -->
 </footer>
 </html>
