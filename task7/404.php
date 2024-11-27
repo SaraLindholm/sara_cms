15d98edd37c8eac3e3a9ab864f8d277a99a0a88b
SaraLindholm <sara.lindholm88@gmail.com>
2024-11-27 09:19:21 +0100

    hade glömt lägga till länkar och sökruta i 404

--- task7/404.php
+++ task7/404.php
@@ -1,5 +1,20 @@
-<?php include 'helper.php';
+<style> <?php include 'style.css'; ?> </style>
+<?php
+include 'helper.php';
 sl_get_header();
-echo '<h4>404: Sidan kunde inte hittas! Kanske har den gömts under en osynlighetsmantel, eller så försvann den genom en portnyckel. Leta vidare, trollstav redo!</h4>';
-sl_get_footer();
+?>
+<div class="not-found-container">
+  <div class="not-found-content">
+    <h3>404: Sidan kunde inte hittas! Kanske har den gömts under en osynlighetsmantel, eller så försvann den genom en portnyckel. Leta vidare, trollstav redo!</h3>
+    <div class="container mt-5">
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Sök" aria-label="Sök">
+      <button class="btn btn-outline-primary" type="submit">Sök</button>
+    </form>
+    <p><a href="index.php">Tillbaka till Start</a></p>
+    <p><a href="nyheter.php">Ta mig till Nyheterna</a></p>
+  </div>
+  </div>
+</div>
+<?php sl_get_footer();
 ?>
